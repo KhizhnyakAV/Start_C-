3645debfdf08a8e48611718a277d8b917fa7c37a
KhizhnyakAV <112726757+KhizhnyakAV@users.noreply.github.com>
2022-10-02 04:39:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Является ли число квадратом другого числа?");
+System.Console.WriteLine("Введите первое число:");
+
+string input = Console.ReadLine();
+int one = int.Parse(input);
+
+System.Console.WriteLine("Введите второе число:");
+
+input = Console.ReadLine();
+int two = int.Parse(input);
+
+//расчёт
+if (one*one == two) {System.Console.WriteLine("Второе число является квадратом первого");};
+if (two*two == one) {System.Console.WriteLine("Первое число является квадратом второго");};
